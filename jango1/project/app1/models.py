dcc2a222ffd7cab15b9bc4a69941957b52b69829
Dhanush N S <135953304+Dhanush-NS@users.noreply.github.com>
2024-05-23 06:35:09 +0530

    Delete jango1 directory

--- jango1/project/app1/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Destination:
-    id: int
-    name: str
-    price: int
--- jango1/project/app1/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Destination
-# Create your views here.
-def index(request):
-    # dest1 = Destination()
-    # dest1.name = "dhanush"
-    # dest1.id = 1
-    # dest1.price = 300
-    return render(request, 'index.html',{'dest1_name': 'dhanush'})
